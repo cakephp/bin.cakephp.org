096e439baec79dc7d93e4a2a6e2f69fad94a7e6c
Mark Story <mark@mark-story.com>
2012-09-03 12:15:01 -0400

    Fix XSS hole in old bin code.

--- View/Helper/CakebinHelper.php
+++ View/Helper/CakebinHelper.php
@@ -1,18 +1,17 @@
 <?php
-App::import('Vendor', 'Geshi');
 App::uses('AppHelper', 'View/Helper');
 App::uses('Hash', 'Utility');
 
 class CakebinHelper extends AppHelper {
 	
-	var $helpers = array('Text');
+	public $helpers = array('Text');
 
 	function aList($array) {
-		if(!empty($array)) {
+		if (!empty($array)) {
 			$array = Hash::extract($array, '{n}.name');
 			if(!empty($array)) {
 				asort($array);
-				return $this->Text->toList(array_values($array));
+				return h($this->Text->toList(array_values($array)));
 			}
 		}
 		return false;
